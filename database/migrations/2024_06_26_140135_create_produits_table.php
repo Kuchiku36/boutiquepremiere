962860c706138b144b6d3bd47016f40a266660e2
veronique derick <derickveronique@gmail.com>
2024-06-26 10:16:31 -0400

    migrate (user,category,produit)

---
+++ database/migrations/2024_06_26_140135_create_produits_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produits', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->string('name', 60);
+            $table->text('description')->nullable() ;
+            $table->float('price')->nullable() ;
+            $table->string('image', 255) ;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produits');
+    }
+};
