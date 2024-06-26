ee63c77ffcc8826eaf4f71b3f2c0db6266232eef
veronique derick <derickveronique@gmail.com>
2024-06-26 11:48:17 -0400

    mise en place des fake data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Produit;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,6 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
+        Category::factory(10)->create();
+        Produit::factory(10)->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
